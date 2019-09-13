9825099414852bd99798fd5b1c5225bc786b3619
David <david.mourereau@gmail.com>
2019-09-14 00:35:35 +0200

    Folder reorganisation

--- deployer/allspark_ansible/test_keycloak.sh
+++
@@ -1,7 +0,0 @@
-docker kill haproxy
-docker kill keycloak
-docker kill keycloak_database
-docker container prune -f 
-docker volume rm allspark_keycloak_postgres_data
-docker volume rm allspark_keycloak_logs
-ansible-playbook -i hosts install.yml --connection=local
